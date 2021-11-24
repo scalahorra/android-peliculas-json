7c0c927d860130e941f52987647c3753dc3eecc7
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-11-24 22:21:31 +0100

    Relleno de implementaciones en el Adapter

--- app/src/main/java/com/example/android_peliculas_json/PeliculaAdapter.java
+++ app/src/main/java/com/example/android_peliculas_json/PeliculaAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android_peliculas_json;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -10,36 +11,56 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class PeliculaAdapter extends RecyclerView.Adapter<PeliculaAdapter.PeliculaHolder> {
 
     private Context context;
-    private List<Pelicula> peliculas;
+    private List<Pelicula> peliculaList;
+
+    PeliculaAdapter(Context context, List<Pelicula> peliculas) {
+        this.context = context;
+        peliculaList = peliculas;
+    }
 
     @NonNull
     @Override
     public PeliculaAdapter.PeliculaHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(context).inflate(R.layout.pelicula, parent, false);
+        return new PeliculaHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PeliculaAdapter.PeliculaHolder holder, int position) {
 
+        Pelicula pelicula = peliculaList.get(position);
+        Glide.with(context).load(pelicula.getPortada()).into(holder.portada);
+        holder.titulo.setText(pelicula.getTitulo());
+        holder.puntuacion.setText(pelicula.getPuntuacion().toString());
+        holder.descripcion.setText(pelicula.getDescripcion());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return peliculaList.size();
     }
 
     public class PeliculaHolder extends RecyclerView.ViewHolder {
 
         ImageView portada;
         TextView titulo, puntuacion, descripcion;
+        ConstraintLayout layout_pelicula;
 
         public PeliculaHolder(@NonNull View itemView) {
             super(itemView);
+
+            portada = itemView.findViewById(R.id.portada);
+            titulo = itemView.findViewById(R.id.titulo_tv);
+            puntuacion = itemView.findViewById(R.id.puntuacion_tv);
+            descripcion = itemView.findViewById(R.id.descripcion_tv);
+            layout_pelicula = itemView.findViewById(R.id.layout_pelicula);
         }
     }
 }
