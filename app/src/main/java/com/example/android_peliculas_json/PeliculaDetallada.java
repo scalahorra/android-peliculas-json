78ed33c9d220a922036e3edf9dbf7af55e24fe30
scalahorra <71397714+scalahorra@users.noreply.github.com>
2021-11-25 19:16:11 +0100

    Correcto funcionamiento de la clase de detalles de película

--- app/src/main/java/com/example/android_peliculas_json/PeliculaDetallada.java
+++ app/src/main/java/com/example/android_peliculas_json/PeliculaDetallada.java
@@ -3,6 +3,10 @@ package com.example.android_peliculas_json;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class PeliculaDetallada extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class PeliculaDetallada extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pelicula_detallada);
+
+        ImageView portada = findViewById(R.id.portada_detallada);
+        TextView titulo = findViewById(R.id.titulo_detallada_tv);
+        TextView puntuacion = findViewById(R.id.valoracion_detallada_tv);
+        TextView descripcion = findViewById(R.id.descripcion_detallada_tv);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String pPortada = bundle.getString("portada");
+        String pTitulo = bundle.getString("titulo");
+        double pPuntuacion = bundle.getDouble("puntuacion");
+        String pDescripcion = bundle.getString("descripcion");
+
+        Glide.with(this).load(pPortada).into(portada);
+        titulo.setText(pTitulo);
+        puntuacion.setText(Double.toString(pPuntuacion));
+        descripcion.setText(pDescripcion);
     }
 }
